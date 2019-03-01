92be6e4a17516a89d664361181ec56fddfe25a47
ShumpeiSenami <kaifenan@gmail.com>
2019-03-01 11:14:26 +0000

    投稿フォームの見た目と投稿ボタン時の/postsのテスト実装

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bulletin-board
+## expressフレームワークを使用した匿名掲示板の作成
+### twitterとgithub を利用した認証
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '掲示板', user: req.user });
 });
 
+router.post('/posts', function(req, res, next){
+    console.log(req.body);
+    handleRedirectPosts(req, res);
+  });
+
+function handleRedirectPosts(req, res){
+  res.writeHead(303, {
+    'Location': '/'
+  });
+  res.end();
+}
+
 module.exports = router;
